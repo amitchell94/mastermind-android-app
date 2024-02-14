3440f5a47816a2877063f15a1cfa682fb4d35636
amitchell94 <andymitchell1@live.co.uk>
2024-02-14 12:13:40 +0000

    upgraded everything to latest versions and API

--- app/src/main/java/com/andy_mitchell/mastermind/activity/MainActivity.java
+++ app/src/main/java/com/andy_mitchell/mastermind/activity/MainActivity.java
@@ -1,7 +1,7 @@
 package com.andy_mitchell.mastermind.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
--- app/src/main/java/com/andy_mitchell/mastermind/activity/SelectColourDialogFragment.java
+++ app/src/main/java/com/andy_mitchell/mastermind/activity/SelectColourDialogFragment.java
@@ -7,9 +7,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import android.widget.Button;
 
 import com.andy_mitchell.mastermind.Colours;
